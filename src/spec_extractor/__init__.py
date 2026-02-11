9b15a308d31e3b33fa25a9e03718b88e169b15c8
kasztelmacko <m.kasztelani@student.uw.edu.pl>
2026-02-12 00:07:26 +0100

    removed unnecesary bloat

--- src/spec_extractor/__init__.py
+++ src/spec_extractor/__init__.py
@@ -1,4 +1,4 @@
 from src.spec_extractor.config.models import SolarPanelSpecs
-from src.spec_extractor.config.config import OllamaConfig
+from src.spec_extractor.config.ollama_config import OllamaConfig
 
 __all__ = ["SolarPanelSpecs", "OllamaConfig"]
